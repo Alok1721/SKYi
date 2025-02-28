e6ae3bd4eb84d88873df49868bc56e266a9b8eae
alok <2022uec1721@mnit.ac.in>
2025-02-28 17:08:50 +0530

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["firebase/auth"], // Add this line
+      external: ["firebase/auth", "firebase/firestore", "firebase/storage"], // Add this line
     },
   },
 });
